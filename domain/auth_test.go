e52a0283f909950f31659f364ad6a952c35814ef
Raphael Berger <raphaelmb@gmail.com>
2023-02-11 14:28:10 -0300

    Auth test

---
+++ domain/auth_test.go
@@ -0,0 +1,102 @@
+package domain
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/raphaelmb/go-twitter-api-graphql"
+	"github.com/raphaelmb/go-twitter-api-graphql/mocks"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAuthService_Register(t *testing.T) {
+	validInput := twitter.RegisterInput{
+		Username:        "test",
+		Email:           "test@gmail.com",
+		Password:        "password",
+		ConfirmPassword: "password",
+	}
+	t.Run("can register", func(t *testing.T) {
+		ctx := context.Background()
+		userRepo := &mocks.UserRepo{}
+		userRepo.On("GetByUsername", mock.Anything, mock.Anything).Return(twitter.User{}, twitter.ErrNotFound)
+		userRepo.On("GetByEmail", mock.Anything, mock.Anything).Return(twitter.User{}, twitter.ErrNotFound)
+		userRepo.On("Create", mock.Anything, mock.Anything).Return(twitter.User{
+			ID:       "123",
+			Username: validInput.Username,
+			Email:    validInput.Email,
+		}, nil)
+		service := NewAuthService(userRepo)
+
+		res, err := service.Register(ctx, validInput)
+		require.NoError(t, err)
+		require.NotEmpty(t, res.AccessToken)
+		require.NotEmpty(t, res.User.ID)
+		require.NotEmpty(t, res.User.Email)
+		require.NotEmpty(t, res.User.Username)
+
+		userRepo.AssertExpectations(t)
+	})
+
+	t.Run("username taken", func(t *testing.T) {
+		ctx := context.Background()
+		userRepo := &mocks.UserRepo{}
+		userRepo.On("GetByUsername", mock.Anything, mock.Anything).Return(twitter.User{}, nil)
+		service := NewAuthService(userRepo)
+
+		_, err := service.Register(ctx, validInput)
+		require.ErrorIs(t, err, twitter.ErrUsernameTaken)
+
+		userRepo.AssertNotCalled(t, "Create")
+		userRepo.AssertExpectations(t)
+	})
+
+	t.Run("email taken", func(t *testing.T) {
+		ctx := context.Background()
+		userRepo := &mocks.UserRepo{}
+		userRepo.On("GetByUsername", mock.Anything, mock.Anything).Return(twitter.User{}, twitter.ErrNotFound)
+		userRepo.On("GetByEmail", mock.Anything, mock.Anything).Return(twitter.User{}, nil)
+		service := NewAuthService(userRepo)
+
+		_, err := service.Register(ctx, validInput)
+		require.ErrorIs(t, err, twitter.ErrEmailTaken)
+
+		userRepo.AssertNotCalled(t, "Create")
+		userRepo.AssertExpectations(t)
+	})
+
+	t.Run("create error", func(t *testing.T) {
+		ctx := context.Background()
+		userRepo := &mocks.UserRepo{}
+		userRepo.On("GetByUsername", mock.Anything, mock.Anything).Return(twitter.User{}, twitter.ErrNotFound)
+		userRepo.On("GetByEmail", mock.Anything, mock.Anything).Return(twitter.User{}, twitter.ErrNotFound)
+		userRepo.On("Create", mock.Anything, mock.Anything).Return(twitter.User{
+			ID:       "123",
+			Username: validInput.Username,
+			Email:    validInput.Email,
+		}, errors.New("an error"))
+		service := NewAuthService(userRepo)
+
+		_, err := service.Register(ctx, validInput)
+		require.Error(t, err)
+
+		userRepo.AssertExpectations(t)
+	})
+
+	t.Run("invalid input", func(t *testing.T) {
+		ctx := context.Background()
+		userRepo := &mocks.UserRepo{}
+		service := NewAuthService(userRepo)
+
+		_, err := service.Register(ctx, twitter.RegisterInput{})
+		require.ErrorIs(t, err, twitter.ErrValidation)
+
+		userRepo.AssertNotCalled(t, "GetByUsername")
+		userRepo.AssertNotCalled(t, "GetByEmail")
+		userRepo.AssertNotCalled(t, "Create")
+		userRepo.AssertExpectations(t)
+	})
+
+}
